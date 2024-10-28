3b0c7ed2e99ba1d09cccdaba9e19519a717e657b
Mia Dietershagen <dietershagen@icom.rwth-aachen.de>
2024-10-28 16:27:43 +0100

    Änderung Variabelname

--- objektpositionierung.py
+++ objektpositionierung.py
@@ -1,18 +1,18 @@
 # The function to calculate the focal length
-def cal_focal(distance, h_camera, h_real):
+def cal_focal(distance, h_pic, h_real):
 # one distance is required to calculate the focal length once (calibration)
     """
     Parameters
     ------------------
     distance : the distance from the camera to the objekt in mm
-    h_camera : the height of the camera in mm
+    h_pic : the height of the object in pixel
     h_real : the height of the object in mm
 
     Returns
     ------------------
     returns the focal length of the used camera
     """
-    return distance * (h_camera / h_real)
+    return distance * (h_pic / h_real)
 
 def cal_distance(h_pic, h_real, focal):
     """
