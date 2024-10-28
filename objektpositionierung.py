3f9bcae0346862b758eb3423ec7bcbbae9183130
Mia Dietershagen <dietershagen@icom.rwth-aachen.de>
2024-10-28 15:35:38 +0100

    test gelöscht

--- objektpositionierung.py
+++ objektpositionierung.py
@@ -1,6 +1,5 @@
 # calculates the focal length
 def cal_focal(distance, height_lens, height_object):
-    print("123")
 # one distance is required to calculate the focal length once (calibration)
     return distance * (height_lens / height_object)
 
