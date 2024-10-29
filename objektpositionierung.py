1e32801e0f42b610b088ecb3efc7c78289a7d5f0
Mia Dietershagen <dietershagen@icom.rwth-aachen.de>
2024-10-29 15:33:27 +0100

    cal_angle

--- objektpositionierung.py
+++ objektpositionierung.py
@@ -1,3 +1,5 @@
+import math
+
 # The function to calculate the focal length
 def cal_focal(distance, h_pic, h_real):
 # one distance is required to calculate the focal length once (calibration)
@@ -21,7 +23,7 @@ def cal_distance(h_pic, h_real, focal):
     ------------------
     h_pic : the height of the object in pixel
     h_real : the height of the object in mm
-    focal : the focal length of the camera in mm
+    focal : the focal length of the camera in pixel
 
     Returns
     ------------------
@@ -29,7 +31,33 @@ def cal_distance(h_pic, h_real, focal):
     """
     return focal * (h_real / h_pic)
 
+# The function calculates the horizontal and vertical angle between the object and the camera
+def cal_angle(x_pic, y_pic, focal):
+    """
+    Parameters
+    -------------------
+    x_pic : the x coordinate of the object in pixel
+    y_pic : the y coordinate of the object in pixel
+    focal : the focal length of the camera in pixel
 
+    Returns
+    --------------------
+    returns the horizontal and vertical angle of the camera in degrees
+    """
+    vertical_angle = math.atan(y_pic/focal)
+    horizontal_angle = math.atan(x_pic/focal)
+    return horizontal_angle, vertical_angle
+
+def cal_position(x_cam_world, y_cam_world, z_cam_world, angle_h, angle_v):
+    """
+    Parameters
+    :param x_cam_world: x coordinate of the camera in mm
+    :param y_cam_world: y coordinate of the camera in mm
+    :param z_cam_world: z coordinate of the camera in mm
+    :param angle_h: horizontal angle between the camera and the object in degrees
+    :param angle_v: vertical angle between the camera and the object in degrees
+    :return: the position of the object in mm
+    """
 
 
 
